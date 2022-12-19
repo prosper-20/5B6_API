3ffbff357c4b14f3df6d6010103d823ce3a38691
prosper <edwardprosper001@gmail.com>
2022-12-19 22:34:24 +0100

    updates

--- django_api/urls.py
+++ django_api/urls.py
@@ -15,21 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.schemas import get_schema_view # new
 from rest_framework import permissions # new
 from drf_yasg import openapi # new
+from drf_yasg.views import get_schema_view
 
-schema_view = get_schema_view( # new
-openapi.Info(
-title="Blog API",
-default_version="v1",
-description="A sample API for learning DRF",
-terms_of_service="https://www.google.com/policies/terms/",
-contact=openapi.Contact(email="hello@example.com"),
-license=openapi.License(name="BSD License"),
-),
-public=True,
-permission_classes=(permissions.AllowAny,),
+schema_view = get_schema_view(
+    openapi.Info(
+    title="Blog API",
+    default_version="v1",
+    description="A sample API for learning DRF",
+    terms_of_service="https://www.google.com/policies/terms/",
+    contact=openapi.Contact(email="hello@example.com"),
+    license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 
